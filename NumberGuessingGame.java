0e00798fcea026e74549d31aae7210afbcd7c51d
Aditya Mahajan <mahajan2005aditya@gmail.com>
2025-02-27 18:53:56 +0530

    NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,4 +1,6 @@
-/* Problem statement:- Create a java program for number guessing.
+/* 
+ Created by Aditya Mahajan from SE computer engineering at PVPIT
+Problem statement:- Create a java program for number guessing.
   1.Generate a random number within a specified range such as 1 to 100
   2.Prompt the user to enter their guess for the generated number
   3.Compare the user's guess with the generated number and provide feedback on whether the guess is correct, too high or too low
